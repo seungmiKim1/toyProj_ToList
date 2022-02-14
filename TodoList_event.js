82f224c5e1f0876bc5f95acb763d6f5e58e0d3ff
brandy <89903766+seungmiKim1@users.noreply.github.com>
2022-02-14 22:29:49 +0900

    220214_토이프로젝트_TodoList구현

--- TodoList_event.js
+++ TodoList_event.js
@@ -1,2 +1,21 @@
 var today = new Date(); 
 document.write(today);
+
+function add_row() { 
+    var my_row = document.getElementById('todoList'); 
+    var row = todoList.insertRow(todoList.rows.length);
+    var cell1=row.insertCell(0);
+    var cell2=row.insertCell(1);
+    var cell3=row.insertCell(2);
+    
+}
+
+function row_delete() { 
+    var my_row = document.getElementById('todoList'); 
+    var row = todoList.deleteRow(todoList.rows.length);
+    var cell1=row.deleteCell(0);
+    var cell2=row.deleteCell(1);
+    var cell3=row.deleteCell(2);
+    
+}
+
